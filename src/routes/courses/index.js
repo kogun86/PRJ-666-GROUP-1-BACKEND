b46add210b497658e29a58a805989e34cd01b78a
Daniil Boiko <boykodaniil58@gmail.com>
2025-05-28 16:10:57 -0400

    Add GET route for courses

--- src/routes/courses/index.js
+++ src/routes/courses/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import getRoutes from './get.js';
 import postRoutes from './post.js';
 
 const router = express.Router();
 
 postRoutes(router);
+getRoutes(router);
 
 export default router;
