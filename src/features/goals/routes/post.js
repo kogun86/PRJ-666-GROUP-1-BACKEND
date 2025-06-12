30b05e0eafc91a374c548138d963f97a0bf9b9e8
Daniil Boiko <boykodaniil58@gmail.com>
2025-06-12 14:52:12 -0400

    Add documentation for POST goals route

--- src/features/goals/routes/post.js
+++ src/features/goals/routes/post.js
@@ -1,6 +1,44 @@
 import { createGoal } from '../goal.controller.js';
 
 export default (router) => {
+  /**
+   * @swagger
+   * /goal:
+   *  post:
+   *   tags:
+   *    - Goals
+   *   summary: Create a new goal for the authenticated user
+   *   description: Create a new goal with the provided details for the authenticated user
+   *   security:
+   *    - bearerAuth: []
+   *   requestBody:
+   *    required: true
+   *    content:
+   *     application/json:
+   *      schema:
+   *       $ref: '#/components/schemas/GoalInput'
+   *   responses:
+   *    '201':
+   *     description: Goal created successfully
+   *     content:
+   *      application/json:
+   *       schema:
+   *        type: object
+   *        required:
+   *         - success
+   *         - goal
+   *        properties:
+   *         success:
+   *          type: boolean
+   *          description: Indicates if the request was successful
+   *          default: true
+   *         goal:
+   *          $ref: '#/components/schemas/Goal'
+   *    '401':
+   *     $ref: '#/components/responses/Unauthorized'
+   *    '500':
+   *     $ref: '#/components/responses/InternalServerError'
+   */
   router.post('/', async (req, res) => {
     const user = req.user.userId;
     const goalData = req.body;
