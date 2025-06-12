f99d09aafa40d10f4178e7948471eb6faccd47e2
Daniil Boiko <boykodaniil58@gmail.com>
2025-06-12 14:52:01 -0400

    Add documentation for GET goals route

--- src/features/goals/routes/get.js
+++ src/features/goals/routes/get.js
@@ -1,6 +1,48 @@
 import { getGoals } from '../goal.controller.js';
 
 export default (router) => {
+  /**
+   * @swagger
+   * /goal:
+   *  get:
+   *   tags:
+   *    - Goals
+   *   summary: Get all goals for the authenticated user
+   *   description: Retrieve all goals associated with the authenticated user.
+   *   security:
+   *    - bearerAuth: []
+   *   parameters:
+   *    - in: path
+   *      name: expandCourse
+   *      schema:
+   *       type: boolean
+   *       description: Whether to expand course details in the response
+   *   responses:
+   *    '200':
+   *     description: Successfully retrieved goals
+   *     content:
+   *      application/json:
+   *       schema:
+   *        type: object
+   *        required:
+   *         - success
+   *         - goals
+   *        properties:
+   *         success:
+   *          type: boolean
+   *          description: Indicates if the request was successful
+   *          default: true
+   *         goals:
+   *          type: array
+   *          items:
+   *            oneOf:
+   *             - $ref: '#/components/schemas/Goal'
+   *             - $ref: '#/components/schemas/GoalExpanded'
+   *    '401':
+   *     $ref: '#/components/responses/Unauthorized'
+   *    '500':
+   *     $ref: '#/components/responses/InternalServerError'
+   */
   router.get('/', async (req, res) => {
     const userId = req.user.userId;
 
